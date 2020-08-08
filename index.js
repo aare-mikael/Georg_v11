54ddad3f994d0a2cf8c58510310f97f1befd7415
Mikael Aare <aaremikael@gmail.com>
2020-08-09 00:49:17 +0200

    Changed how commands were applied, made a folder-structure using module.exports

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
+// fs is Node's native file system module;
+const fs = require('fs');
+
 // Requires the discord.js module, which this bot is built by;
 const Discord = require('discord.js');
+
+// Requires the prefix specified in config.json, to avoid issues where a command name is randomly said without meaning to invoke the command;
+const { prefix, token } = require('./config.json');
 
 // Creates a new Discord client, essentially this is the bot;
 const client = new Discord.Client();
 
+// Creates a collection with all the commands dealing with text, like "delete" and "hangman";
+client.textcommands = new Discord.Collection();
+
+// Declares an array which contains all files in textcommands which ends in .js, which is Javascript-files;
+const textcommandFiles = fs.readdirSync('./textcommands').filter(file => file.endsWith('.js'));
+
+for (const file of textcommandFiles) {
+    // Requires all files in textcommands;
+    const textcommand = require(`./textcommands/${file}`);
+
+    // Set a new item in the collection with the key as the commandname and the value as the exported module;
+    client.textcommands.set(textcommand.name, textcommand);
+}
 
 
 // Hide your token at all costs;
-client.login("NzQxNzAzOTIxODc3MTIzMTY0.Xy7bmw.JdnS0O6PmdyzKWnDHhu66yW8UQg");
+client.login(token);
 
 // When client is ready, this code will be run and will only trigger once after logging in;
 client.once('ready', () => {
@@ -17,7 +36,15 @@ client.once('ready', () => {
 
 // Makes the bot react when a textmessage pops into a channel it has access to;
 client.on('message', message => {
-    if(message.content === 'test') {
+    
+    // Forces the bot to return immediately when the message doesn't contain the specified prefix, which saves resources;
+    if (!message.content.startsWith(prefix)) return;
+
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'test') {
         message.channel.send('test working');
     }
-})
+});
