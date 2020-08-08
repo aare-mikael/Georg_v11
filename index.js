afd637937e726505f30c61f266d5dd297a4fe129
Mikael Aare <aaremikael@gmail.com>
2020-08-08 22:57:09 +0200

    Cleaned up code and added comments for easier reading

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
+// Requires the discord.js module, which this bot is built by;
 const Discord = require('discord.js');
+
+// Creates a new Discord client, essentially this is the bot;
 const client = new Discord.Client();
 
-// Hide at all costs;
+
+
+// Hide your token at all costs;
 client.login('NoneOfYourBusiness');
 
+// When client is ready, this code will be run and will only trigger once after logging in;
 client.once('ready', () => {
     console.log('Ready!');
     client.user.setActivity('with your heart');
 });
 
+// Makes the bot react when a textmessage pops into a channel it has access to;
 client.on('message', message => {
     if(message.content === 'test') {
         message.channel.send('test working');
     }
 })
-
-
