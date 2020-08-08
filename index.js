16f9a87b14f7d22be56861f6e917b995403a6542
Mikael Aare <aaremikael@gmail.com>
2020-08-08 23:20:19 +0200

    New and better method of tracking commands in index.js

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 // Requires the discord.js module, which this bot is built by;
 const Discord = require('discord.js');
 
+// Requires the config file, which can be used for sensitive info;
+const { prefix, token } = require('./config.json');
+
 // Creates a new Discord client, essentially this is the bot;
 const client = new Discord.Client();
 
 
 
-// Hide your token at all costs;
-client.login('NoneOfYourBusiness');
+// Hide your token at all costs, which is what I've done here;
+client.login(token);
 
 // When client is ready, this code will be run and will only trigger once after logging in;
 client.once('ready', () => {
@@ -15,9 +18,12 @@ client.once('ready', () => {
     client.user.setActivity('with your heart');
 });
 
-// Makes the bot react when a textmessage pops into a channel it has access to;
-client.on('message', message => {
-    if(message.content === 'test') {
-        message.channel.send('test working');
-    }
-})
+// Makes the bot immediately stop reading upon finding that the message doesnt start with the prefix specified in config.json, which saves power and time;
+if (!message.content.startsWith(prefix)) return;
+
+const args = message.content.slice(prefix.length).trim();
+const command = args.shift().toLowerCase();
+
+if(message.content == "test a feature") {
+    message.channel.send(command);
+};
