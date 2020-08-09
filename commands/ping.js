b46c3b3260df94fc9912a10a5083120fe76ac50c
Mikael Aare <aaremikael@gmail.com>
2020-08-09 13:01:28 +0200

    Implemented cooldowns when using commands

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,7 @@
 module.exports = {
 	name: 'ping',
 	description: 'Ping!',
+	cooldown: 5,
 	args: true,
 	usage: "<user> <role>",
 	execute(message, args) {
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const Discord = require('discord.js');
 
 // Requires the prefix specified in config.json, to avoid issues where a command name is randomly said without meaning to invoke the command;
 const { prefix, token } = require('./config.json');
+const { cooldown } = require('./commands/ping');
 
 // Creates a new Discord client, essentially this is the bot;
 const client = new Discord.Client();
@@ -24,6 +25,7 @@ for (const file of commandFiles) {
     client.commands.set(command.name, command);
 }
 
+const cooldowns = new Discord.Collection();
 
 // Hide your token at all costs;
 client.login(token);
@@ -46,6 +48,10 @@ client.on('message', message => {
 
     const command = client.commands.get(commandName)
 
+    if (command.guildOnly && message.channel.type !== 'text') {
+        return message.reply('I can\'t execute that command inside DMs!');
+    }    
+
     if(command.args && !args.length) {
         let reply = message.channel.send(`You didn't provide any arguments, ${message.author}!`)
 
@@ -58,6 +64,37 @@ client.on('message', message => {
         return message.channel.send(reply);
     }
 
+    // This enters a command into an array of commands recently used, to limit spam and so forth;
+    if (!cooldowns.has(command.name)) {
+        cooldowns.set(command.name, new Discord.Collection());
+    }
+
+    // Just retrieves a variable with the time as of right now;
+    const now = Date.now();
+
+    // Retrieves the timestamp of a cooldown recently used and the author who used it;
+    const timestamps = cooldowns.get(command.name);
+
+    // Sets the cooldown amount in seconds from milliseconds;
+    // First checks if the cooldown amount has been specified in the command file, if not the cooldown defaults to 3 seconds;
+    const cooldownAmount = (command.cooldown || 3) * 1000;
+
+    // Checks if the author recently used the command, and sets the expirationtime;
+    if (timestamps.has(message.author.id)) {
+        const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+        // Checks if the command can be used again by the author, meaning that the expiration time has expired;
+        if (now < expirationTime) {
+            const timeLeft = (expirationTime - now) ;
+            return message.reply(`please wait ${timeLeft.toFixed(1)} more milliseconds before using the \`${command.name}\` command again!`);
+        }
+    }
+
+    // Just to make sure the author ID of a used command gets deleted from the cooldown array after time has expired;
+    timestamps.set(message.author.id, now);
+    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+    // Tries to execute the command;
     try {
         command.execute(message, args)
     } catch (error) {
