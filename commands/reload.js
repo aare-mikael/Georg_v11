9f97dabdfaccb42f26d8ec50990f20a1c3a6154b
aare-mikael <aaremikael@gmail.com>
2020-09-10 20:10:59 +0200

    :zap:

--- commands/reload.js
+++ commands/reload.js
@@ -1,41 +1,7 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-	module.exports = {
-=======
-<<<<<<< HEAD
 module.exports = {
     name: "reload",
     description: "Reloads the command you specify",
     usage: "[reload command]",
-    execute(message, args) {
-        if (!args.length) return message.channel.send(`You didn't pass any command to reload, ${message.author}!`);
-            
-        const commandName = args[0].toLowerCase();
-        
-        const command = message.client.commands.get(commandName) || message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-
-		if (!command) return message.channel.send(`There is no command with name or alias \`${commandName}\`, ${message.author}!`);
-
-		delete require.cache[require.resolve(`./${command.name}.js`)];
-
-		try {
-			const newCommand = require(`./${command.name}.js`);
-			message.client.commands.set(newCommand.name, newCommand);
-			message.channel.send(`Command \`${command.name}\` was reloaded!`);
-		} catch (error) {
-			console.log(error);
-			message.channel.send(`There was an error while reloading a command \`${command.name}\`:\n\`${error.message}\``);
-		}
-	},
-=======
-=======
->>>>>>> c931b7fa109dadd1df63b9bf538aae1ac6af8524
-module.exports = {
->>>>>>> b6b4272642284771e27439bf2f514f7d2bc8f8ff
-    name: "reload",
-    description: "Reloads the command you specify",
-	usage: "[reload command]",
-	category: "Private",
     execute(message, args) {
         if (!args.length) return message.channel.send(`You didn't pass any command to reload, ${message.author}!`);
             
--- utilities/YoutubeEmbed.js
+++ utilities/YoutubeEmbed.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const Discord = require('discord.js');
 
 module.exports = function embed(message, results) {
@@ -26,39 +25,4 @@ module.exports = function embed(message, results) {
         .setFooter('Georg™ ©2020', 'https://i.imgur.com/wSTFkRM.png');
 
     return Embed;
-
-//    channel.send(Embed);
-
-=======
-const Discord = require('discord.js');
-
-module.exports = function embed(message, results) {
-
-    let rMin = Math.floor(results.duration / 60);
-    let rSek = results.duration % 60;
-    let tid = (rMin + ":" + rSek);
-
-    const Embed = new Discord.MessageEmbed()
-        .setColor('#6f4c78')
-        .setTitle(results.title)
-        .setURL(results.link)
-        .setAuthor('Georg Music', 'https://i.ibb.co/8sbpxpw/GEORG.png', 'https://discord.com/api/oauth2/authorize?client_id=741703921877123164&permissions=2147483639&scope=bot')
-        .setDescription(results.description)
-        .setThumbnail(results.thumbnail)
-/*        .addFields(
-            { name: 'Regular field title', value: 'Some value here' },
-            { name: '\u200B', value: '\u200B' },
-            { name: 'Inline field title', value: 'Some value here', inline: true },
-            { name: 'Inline field title', value: 'Some value here', inline: true },
-        ) */
-        .addField('Duration', tid, true)
-//        .setImage(results.thumbnail)
-        .setTimestamp() 
-        .setFooter('Georg™ ©2020', 'https://i.imgur.com/wSTFkRM.png');
-
-    return Embed;
-
-//    channel.send(Embed);
-
->>>>>>> 162081be50e93303004d213456a09e9250e98d2f
-    }
+}
