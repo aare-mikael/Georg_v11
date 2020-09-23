7326be3d63d40ca3ffe7745b78bfe4f504b95201
aare-mikael <aaremikael@gmail.com>
2020-09-23 14:59:23 +0200

    Experiment didn't work, reverted to previous state

--- commands/georg.js
+++ commands/georg.js
@@ -6,19 +6,13 @@ module.exports = {
     // Returnerer tidlig om meldingen er sendt av en bot;
     if (message.author.bot) return;
 
-    // Trash#8640
-    var trashID = "227520275132973056";
-
     var voiceChannel = message.member.voice.channel;
-    console.log(voiceChannel);
 
     // Returnerer error-message om brukeren ikkje er i en voice-kanal
-    if (!voiceChannel && message.author.id != trashID) {
+    if (!voiceChannel) {
         message.channel.send("You have to be in a voice channel to make this command work. Join a voice channel and try again!");
         return;
-    } else if (!voiceChannel && message.author.id == trashID) {
-        var voiceChannel = args[0];
-    }
+    };
 
     voiceChannel.join().then(connection => {
     const dispatcher = connection.play('./mp3/georg.mp3');
