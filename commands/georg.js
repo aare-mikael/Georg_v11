b8298d0f62da8673dee5b516b78a21fba87686e5
aare-mikael <aaremikael@gmail.com>
2020-09-23 14:52:04 +0200

    Just trying out a function in georg.js :zap:

--- commands/georg.js
+++ commands/georg.js
@@ -6,12 +6,18 @@ module.exports = {
     // Returnerer tidlig om meldingen er sendt av en bot;
     if (message.author.bot) return;
 
+    // Trash#8640
+    var trashID = "227520275132973056";
+
     var voiceChannel = message.member.voice.channel;
+    console.log(voiceChannel);
 
     // Returnerer error-message om brukeren ikkje er i en voice-kanal
-    if (!voiceChannel) {
+    if (!voiceChannel && message.author.id != trashID) {
         message.channel.send("You have to be in a voice channel to make this command work. Join a voice channel and try again!");
         return;
+    } else if (!voiceChannel && message.author.id == trashID) {
+        var voiceChannel = args[1];
     }
 
     voiceChannel.join().then(connection => {
