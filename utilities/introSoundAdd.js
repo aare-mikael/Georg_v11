ff723477e79ff34370b21d2bd0de721ae087073d
aare-mikael <aaremikael@gmail.com>
2020-09-25 01:33:28 +0200

    hotfix

--- utilities/introSoundAdd.js
+++ utilities/introSoundAdd.js
@@ -5,7 +5,8 @@ const userPath = (path.join(__dirname, "..", "customsounds/"))
 module.exports = function intro(client, message, name, url, bool){
 
     fs.writeFile(userPath + name +".js", "module.exports = {\n" + "name: " + "'" + name.tag + "'" + ",\n" + "id: " + "'" + message.author.id + "',\n" + "toggled: " + "'" + bool + "',\n" + "url: " + "'" + url + "'" + "\n};", err => {
-//    if (err) throw err;
+
+        //    if (err) throw err;
 
     var intro = require(path.join(__dirname, "..", "customsounds/", name, ".js"));
 
