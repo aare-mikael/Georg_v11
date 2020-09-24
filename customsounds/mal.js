836d6c5edc1694529b51ca1de6f4cded3f8b4cdb
aare-mikael <aaremikael@gmail.com>
2020-09-25 00:12:05 +0200

    hotfix

--- customsounds/mal.js
+++ customsounds/mal.js
@@ -1,7 +1,6 @@
 module.exports = {
     name: 'Mal#1234',
     id: '123456789',
-    description: `${user.username}'s custom intro sound!`,
     toggled: 'true',
     url: 'https://www.myinstants.com/media/sounds/so-long-gayboys.mp3'
 }; 
