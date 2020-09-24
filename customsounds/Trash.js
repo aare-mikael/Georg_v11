e5e147d281edeed86990fbb85afada44f6c46c1a
aare-mikael <aaremikael@gmail.com>
2020-09-25 00:10:50 +0200

    hotfix

--- customsounds/Trash.js
+++ customsounds/Trash.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'Trash.#8640',
     id: '227520275132973056',
-    description: `${user.username}'s custom intro sound!`,
+    description: `${name} custom intro sound!`,
     toggled: 'true',
     url: 'https://www.myinstants.com/media/sounds/so-long-gayboys.mp3'
 }; 
