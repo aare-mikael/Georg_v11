041568da1f164861037141e3a6b9eab03f9bda46
aare-mikael <aaremikael@gmail.com>
2020-09-25 00:14:12 +0200

    hotfix

--- utilities/introSoundAdd.js
+++ utilities/introSoundAdd.js
@@ -1,3 +1,14 @@
-module.exports = {
-    
+var path = require("path")
+const fs = require("fs")
+const userPath = (path.join(__dirname, "..", "customsounds"))
+
+module.exports = function intro(bot, msg, name, url, bool){
+
+    //fs.writeFile()
+    if (err) throw err;
+
+    var intro;
+
+    bot.introSound.set()
+
 }
