c0e44be42c126491a2305baef7e4b63ca53d35ea
aare-mikael <aaremikael@gmail.com>
2020-09-25 01:05:51 +0200

    Completed function :zap:

--- utilities/introSoundAdd.js
+++ utilities/introSoundAdd.js
@@ -1,14 +1,22 @@
 var path = require("path")
 const fs = require("fs")
-const userPath = (path.join(__dirname, "..", "customsounds"))
+const userPath = (path.join(__dirname, "..", "customsounds/"))
 
-module.exports = function intro(bot, msg, name, url, bool){
+module.exports = function intro(client, message, name, url, bool){
 
-    //fs.writeFile()
+    fs.writeFile(userPath + name, "module.exports = {\n" + "name: " + "'" + name.tag + "'" + ",\n" + "id: " + "'" + message.author.id + "',\n" + "toggled: " + "'" + bool + "',\n" + "url: " + "'" + url + "'" + "\n};");
+
+    module.exports = {
+        name: 'Trash.#8640',
+        id: '227520275132973056',
+        toggled: 'true',
+        url: 'https://www.myinstants.com/media/sounds/so-long-gayboys.mp3'
+    }; 
+
     if (err) throw err;
 
-    var intro = "";
+    var intro = require(path.join(usersPath, name, ".js"));
 
-    bot.introSound.set()
+    client.introSound.set(intro.id, intro)
 
 }
