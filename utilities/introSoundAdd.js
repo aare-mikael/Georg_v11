c589d3b42bd48707bd9bd73c93996740f4e61d81
aare-mikael <aaremikael@gmail.com>
2020-09-25 01:06:40 +0200

    hotfix

--- utilities/introSoundAdd.js
+++ utilities/introSoundAdd.js
@@ -6,15 +6,6 @@ module.exports = function intro(client, message, name, url, bool){
 
     fs.writeFile(userPath + name, "module.exports = {\n" + "name: " + "'" + name.tag + "'" + ",\n" + "id: " + "'" + message.author.id + "',\n" + "toggled: " + "'" + bool + "',\n" + "url: " + "'" + url + "'" + "\n};");
 
-    module.exports = {
-        name: 'Trash.#8640',
-        id: '227520275132973056',
-        toggled: 'true',
-        url: 'https://www.myinstants.com/media/sounds/so-long-gayboys.mp3'
-    }; 
-
-    if (err) throw err;
-
     var intro = require(path.join(usersPath, name, ".js"));
 
     client.introSound.set(intro.id, intro)
