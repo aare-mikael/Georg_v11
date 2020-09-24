467781d26a8667dcd811201092a2b75648b1024c
aare-mikael <aaremikael@gmail.com>
2020-09-25 01:27:59 +0200

    :zap: bugfix

--- utilities/introSoundAdd.js
+++ utilities/introSoundAdd.js
@@ -4,10 +4,10 @@ const userPath = (path.join(__dirname, "..", "customsounds/"))
 
 module.exports = function intro(client, message, name, url, bool){
 
-    fs.writeFile(userPath + name, "module.exports = {\n" + "name: " + "'" + name.tag + "'" + ",\n" + "id: " + "'" + message.author.id + "',\n" + "toggled: " + "'" + bool + "',\n" + "url: " + "'" + url + "'" + "\n};", err => {
+    fs.writeFile(userPath + name +".js", "module.exports = {\n" + "name: " + "'" + name.tag + "'" + ",\n" + "id: " + "'" + message.author.id + "',\n" + "toggled: " + "'" + bool + "',\n" + "url: " + "'" + url + "'" + "\n};", err => {
     if (err) throw err;
 
-    var intro = require(path.join(userPath, `${name}.js`));
+    var intro = require(path.join(userPath, name, ".js"));
 
     client.intro.set(intro.id, intro)
     });
