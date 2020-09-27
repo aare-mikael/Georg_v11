f61ba2323fc4b6f312c7125ff2a906e2cfd477b6
aare-mikael <aaremikael@gmail.com>
2020-09-27 11:50:09 +0200

    Changes in the insults-array :zap:

--- commands/roast.js
+++ commands/roast.js
@@ -1,11 +1,10 @@
 var insults = [
     "du får Sid til å sjå ut som han e på samme nivå som Stephen Hawking",
-    "du e den personen som trekke ut visdomstenner for å lage plass til fleire kuka",
     "du e den personen som memorisera den seksuelle lavalderen i alle land øst for Afghanistan",
     "å måtte høre på deg snakke e som å drikke appelsinjuice og pusse tenna rett etterpå",
     "hjerna di e så liten at den sprette rundt inni skallen din på samme måte som en DVD-screensaver",
     "du e den personen som går på byn med 2 kondoma, og kjeme heim med 5",
-    "du ser ut som du ikkje kunne talt til 11 uten å ta av deg sokkane, men når du gjør det kan du telle til 23",
+    "du ser ut som du ikkje kan telle til 11 uten å ta av deg sokkane, men når du gjør det kan du telle til 23",
     "du ser ut som den personen som jobba som lærer på barneskulen bare for å sjekke opp single mødre",
     "den dagen eg skal ha meg unga, skal eg spørre foreldra dine ka posisjon dei knulla i før dei fikk deg, så eg kan unngå den",
     "det e over 300 000 ord i det norske språket, og likevel kunne eg aldri satt sammen nok ord til å forklare kor mykje eg har lyst til å slå deg med et balltre",
@@ -35,19 +34,14 @@ module.exports = {
     //Endrer alle meldinger til lowercase for å hindre case-sensitive commands;
     const msg = message.content.toLowerCase();
 
-    // Finner ut ken som er nevnt i meldinga og tagger dei;
+    // Henter ut personen som skal roastes;
     var bruker = args[0];
 
     var antallInsults = insults.length;
     var insult = Math.floor(Math.random() * antallInsults - 1) + 1;
     var spy =  insults[insult];
-
-      // Kommentert ut enn så lenge;
-//      message.channel.bulkDelete(1);
       
     // Spyr ut en tilfeldig insult fra insults[];      
       message.channel.send(bruker + ", " + spy);
-
-
     },
 };
