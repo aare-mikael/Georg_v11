500c1aa8b3c5ab1aa0f1865fa293a1ade32d1610
aare-mikael <aaremikael@gmail.com>
2020-09-27 19:38:02 +0200

    Upgraded looks in GuildJoinEmbed :zap:

--- utilities/GuildJoinEmbed.js
+++ utilities/GuildJoinEmbed.js
@@ -6,18 +6,17 @@ module.exports = function embed() {
 
     const Embed = new Discord.MessageEmbed()
         .setColor('#6f4c78')
-        .setTitle('Hello, this is a "How to" on how I work and can help you in your server!')
+        .setTitle('This is a "How to" on how I work and can help you in your server!')
         .setAuthor('Georg', 'https://i.ibb.co/8sbpxpw/GEORG.png', 'https://discord.com/api/oauth2/authorize?client_id=741703921877123164&permissions=2147483639&scope=bot')
-        .setDescription(`My prefix is this: ${prefix}`)
+        .setDescription(`My prefix is this: "${prefix}"`)
         .addFields(
-            { name: 'My personal recommendation:', value: 'The first command you use should be the **help** command!' },
-            { name: '\u200B', value: '\u200B' },
-            { name: 'Inline field title', value: 'Some value here', inline: true },
-            { name: 'Inline field title', value: 'Some value here', inline: true },
+            { value: 'The first command you use should be the **help** command!' },
+            { value: 'I can moderate messages, join voice and play audio clips from youtube and more!' },
+            { value: 'All you have to do is use the help-command, and I should slide right into your DM\'s with more info!' },
         ) 
 //        .setImage(results.thumbnail)
         .setTimestamp() 
-        .setFooter('Georg™ ©2020', 'https://i.imgur.com/wSTFkRM.png');
+        .setFooter('Georg™ ©2020', 'https://i.ibb.co/8sbpxpw/GEORG.png');
 
     return Embed;
 }
