9e8b0718f0c9247a26ceedc9822bdc1d1f4c3d78
aare-mikael <aaremikael@gmail.com>
2020-10-12 00:09:24 +0200

    Fetches a random trump-quote

--- commands/trump.js
+++ commands/trump.js
@@ -6,8 +6,12 @@ module.exports = {
         // Returnerer tidlig om meldingen er sendt av en bot;
         if (message.author.bot) return;
 
-        
+        // Retrieve a random quote from https://api.tronalddump.io/random/quote;
+        let obj = await (await fetch("https://api.tronalddump.io/random/quote")).json();
 
+        let quote = obj.value;
+
+        message.channel.send(quote);
 
     },
 };
