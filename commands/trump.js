8bbd34bfc339bcef94070247f095ee58031400f2
aare-mikael <aaremikael@gmail.com>
2020-10-11 23:56:06 +0200

    :zap: https://www.tronalddump.io/ :zap:

---
+++ commands/trump.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: 'trump',
+    description: "This fetches a random idiotic quote of our lord and saviour, Donald Trump.",
+    category: "Text",
+    execute(message, args) {
+        // Returnerer tidlig om meldingen er sendt av en bot;
+        if (message.author.bot) return;
+
+        
+
+
+    },
+};
