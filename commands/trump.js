a0c6ad32ec06eb0caf4080ba6f0d9d7d52db7623
aare-mikael <aaremikael@gmail.com>
2020-10-12 00:28:09 +0200

    async execute

--- commands/trump.js
+++ commands/trump.js
@@ -4,14 +4,15 @@ module.exports = {
     name: 'trump',
     description: "This fetches a random idiotic quote of our lord and saviour, Donald Trump.",
     category: "Text",
-    execute(message, args) {
+    async execute(message, args) {
         // Returnerer tidlig om meldingen er sendt av en bot;
         if (message.author.bot) return;
 
         // Retrieve a random quote from https://api.tronalddump.io/random/quote;
 //        let obj = await (await fetch("https://api.tronalddump.io/random/quote")).json();
 
-        let obj = fetch("https://api.tronalddump.io/random/quote");
+        let obj = await (await fetch("https://api.tronalddump.io/random/quote")).json();
+
         console.log(obj);
 
         let quote = obj.value;
