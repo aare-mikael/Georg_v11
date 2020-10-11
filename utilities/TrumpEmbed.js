7f8f6cc9cdfb73d584bf2c31854eb6212c999792
aare-mikael <aaremikael@gmail.com>
2020-10-12 00:49:16 +0200

    Fixed embed apperance

--- utilities/TrumpEmbed.js
+++ utilities/TrumpEmbed.js
@@ -7,9 +7,9 @@ module.exports = function embed(message, obj) {
         .setTitle('A random Twitter status from our lord and saviour!')
         .setAuthor('Georg', 'https://i.ibb.co/8sbpxpw/GEORG.png', 'https://discord.com/api/oauth2/authorize?client_id=741703921877123164&permissions=2147483639&scope=bot')
         .addFields(
-            { name: "--------------------", value: obj.value },
-            { name: "--------------------", value: obj.appeared_at },
-            { name: "--------------------", value: obj._embedded.source[0].url },
+            { name: "Tweet:", value: obj.value },
+            { name: "Tweeted at:", value: obj.appeared_at },
+            { name: "Url", value: obj._embedded.source[0].url },
         ) 
         .setTimestamp() 
         .setFooter('Georg™ ©2020', 'https://i.ibb.co/8sbpxpw/GEORG.png');
