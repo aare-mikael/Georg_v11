6b5f207f3494c3885fce8c257898df32b9a010eb
aare-mikael <aaremikael@gmail.com>
2020-10-12 00:46:44 +0200

    hotfix url

--- commands/trump.js
+++ commands/trump.js
@@ -16,7 +16,7 @@ module.exports = {
 
         let obj = await (await fetch("https://api.tronalddump.io/random/quote")).json();
         console.log(obj)
-        console.log(obj.source.url);
+        console.log(obj._embedded.source[0].url);
 
         message.channel.send(embed(message, obj));
 
