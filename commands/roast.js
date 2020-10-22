e16aaf518bb84cd08abcdd3278c66dcb98f505a0
aare-mikael <aaremikael@gmail.com>
2020-10-22 23:06:22 +0200

    hotfix 5

--- commands/roast.js
+++ commands/roast.js
@@ -36,7 +36,7 @@ module.exports = {
     const msg = message.content.toLowerCase();
 
     if ( !args[0] ) {
-      var bruker = message.author.tag;
+      var bruker = message.author.username;
     } else {
         // Henter ut personen som skal roastes;
         var bruker = args[0];
