853b868a410b9488e7b70a037450f5c7f34a4d71
aare-mikael <aaremikael@gmail.com>
2020-11-03 15:29:21 +0100

    Bug fix

--- utilities/introPlayer.js
+++ utilities/introPlayer.js
@@ -1,10 +1,10 @@
-module.exports = function play(voiceChannel, link, client){
+module.exports = function play(message, args, voiceChannel, link, client){
 
     if(voiceChannel == undefined || voiceChannel == null) return;
 
     let vol = 0.5;
  
-    voiceChannel.join().then(connection => {
+    message.member.voice.channel.join().then(connection => {
         const dispatcher = connection.play(link, { volume: vol } );
         dispatcher.on('finish', () => voiceChannel.leave());
     }).catch(err => console.log(err));
