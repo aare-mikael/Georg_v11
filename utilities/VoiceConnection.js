ec2b73ce9ac8abefbd741af90ffb2b5489f57066
aare-mikael <aaremikael@gmail.com>
2020-12-13 20:12:43 +0100

    finish -> end

--- utilities/VoiceConnection.js
+++ utilities/VoiceConnection.js
@@ -2,6 +2,6 @@ module.exports = function VoiceConnection(voiceChannel, audio){
 
     voiceChannel.join().then(connection => {
         const dispatcher = connection.play(audio);
-        dispatcher.on('finish', () => dispatcher.destroy());
+        dispatcher.on('end', () => dispatcher.destroy());
     }).catch(err => console.log(err));
 };
