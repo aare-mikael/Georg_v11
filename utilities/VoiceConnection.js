ebac8bd02d5cbc5626b3ae5761f848ae2c6c2ebb
aare-mikael <aaremikael@gmail.com>
2020-12-13 20:06:37 +0100

    hotfix

--- utilities/VoiceConnection.js
+++ utilities/VoiceConnection.js
@@ -1,7 +1,5 @@
 module.exports = function VoiceConnection(voiceChannel){
 
-    var voiceChannel = message.member.voice.channel;
-
     voiceChannel.join().then(connection => {
         const dispatcher = connection.play();
         dispatcher.on('finish', () => dispatcher.destroy());
