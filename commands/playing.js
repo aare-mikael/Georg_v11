fc22cc485e8050c125cbf415879ba7be6682e415
aare-mikael <aaremikael@gmail.com>
2020-12-15 12:29:04 +0100

    Created playing.js

---
+++ commands/playing.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: 'playing',
+	description: 'Check what music is playing!',
+	category: 'Voice',
+	cooldown: 5,
+	execute(message) {
+		const serverQueue = message.client.queue.get(message.guild.id);
+		if (!serverQueue) return message.channel.send('There is nothing playing.');
+		return message.channel.send(`🎶 Now playing: **${serverQueue.songs[0].title}**`);
+	}
+};
