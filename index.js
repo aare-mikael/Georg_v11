0fdd1667c6c936e403cf156beed1b111d82c1e70
aare-mikael <aaremikael@gmail.com>
2020-12-28 12:28:40 +0100

    on -> once

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ const cooldowns = new Discord.Collection();
 client.login(process.env.token);
 
 // When client is ready, this code will be run and will only trigger once after logging in;
-client.on('ready', () => {
+client.once('ready', () => {
     console.log('I solemnly swear I am up to no good.');
 //    client.user.setActivity('', { type: 'LISTENING' }); // Listening automatically adds "to" behind it, so this displays "Listening to xxx";
 //    client.user.setActivity('Christmas music 24/7', { type: 'PLAYING' }); // This displays "Playing with your heart";
