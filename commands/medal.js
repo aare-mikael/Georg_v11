7d60f228a7c696daecfe640c65a46fd864b21e7f
Mikael-Aare <mikaelaare@icloud.com>
2021-03-18 13:30:52 +0100

    Added all the other clips that exists so far :zap:

--- commands/medal.js
+++ commands/medal.js
@@ -1,8 +1,28 @@
 var videos = [
-// Her skal medal-linkene
+    // Her skal medal-linkene;
     "https://medal.tv/clips/43950779/d1337xyvLTRY",
-    
-    ]
+    "https://medal.tv/clips/43930190/d1337lRt4yTl",
+    "https://medal.tv/clips/43668198/FMiQYVOwgzD2",
+    "https://medal.tv/clips/26347213/briRZhKwdcgT",
+    "https://medal.tv/clips/28280817/lG95X6ILtQ4Q",
+    "https://medal.tv/clips/37539268/MVxZVCiBrpyB",
+    "https://medal.tv/clips/41634880/d13376X6PlJ1",
+    "https://medal.tv/clips/44580269/d1337eF6Gk4L",
+    "https://medal.tv/clips/44580128/d1337yDbHTJf",
+    "https://medal.tv/clips/44580684/d1337PKXceoM",
+    "https://medal.tv/clips/44662156/d1337GCkYzKi",
+    "https://medal.tv/clips/44834832/d1337kb7ry2A",
+    "https://medal.tv/clips/44834832/d1337DLcmicc",
+    "https://medal.tv/clips/44834859/d1337VZzwwfC",
+    "https://medal.tv/clips/45186689/d13373m6Sr1R",
+    "https://medal.tv/clips/36359599/d1337S3XO5Ku",
+    "https://medal.tv/clips/45348679/d1337brP3FVC",
+    "https://medal.tv/clips/45911244/d1337ahWniO3",
+    "https://medal.tv/clips/46163782/d13378CIpPjq",
+    "https://medal.tv/clips/46348453/d1337paAAvye",
+    "https://medal.tv/clips/46450492/d1337wLepf5K",
+    "https://medal.tv/clips/46450787/d1337f8UeCNV"
+    ];
 
 module.exports = {
     name: 'medal',
