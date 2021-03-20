e92a263a007177cdef3c9d223f1415a90e7d3ed8
Mikael-Aare <mikaelaare@icloud.com>
2021-03-20 21:06:58 +0100

    :zap: :zap:

--- commands/reload.js
+++ commands/reload.js
@@ -1,6 +1,7 @@
 module.exports = {
     name: "reload",
     description: "Reloads the command you specify",
+	category: 'Private',
     usage: "[reload command]",
     execute(message, args) {
         if (!args.length) return message.channel.send(`You didn't pass any command to reload, ${message.author}!`);
