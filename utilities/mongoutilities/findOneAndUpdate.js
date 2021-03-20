dab5737484ad48a4d693129f156e608e007f5243
Mikael-Aare <mikaelaare@icloud.com>
2021-03-20 18:04:37 +0100

    :zap: :zap:

--- utilities/mongoutilities/findOneAndUpdate.js
+++ utilities/mongoutilities/findOneAndUpdate.js
@@ -6,11 +6,14 @@ const mongo = require('./mongo');
 module.exports = async (message) => {
     await mongo().then(async (mongoose) => {
         try {
-            await discordUsers.findOneAndUpdate({
+            const result = await discordUsers.findOneAndUpdate({
                 user: message.mentions.users.first() || message.author,
-
+                // Sette inn kriterie for at personen ikkje har introlyd allerede?
             }, {
-
+                // Oppdatere introlyd kanskje?
+            }, {
+                upsert: true,
+                new: true,
             })
         } finally {
             mongoose.connection.close();
