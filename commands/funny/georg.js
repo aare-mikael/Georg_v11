c3d45ff9a0e1666c5388f4e88aaf34541541424b
Mikael-Aare <mikaelaare@icloud.com>
2021-03-21 14:39:30 +0100

    :zap:

--- commands/funny/georg.js
+++ commands/funny/georg.js
@@ -3,8 +3,8 @@ const VoiceJoin = require("../../utilities/VoiceJoin");
 module.exports = {
     name: 'georg',
     description: "Joins voice channel and plays georg.mp3",
-    category: "Voice",
-    execute(message, args) {
+    category: "Funny",
+    callback: (message) => {
 
         const audio = '../../mp3/georg.mp3';
 
