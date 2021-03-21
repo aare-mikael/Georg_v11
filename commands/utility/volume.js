bb2150071b509674efbd8c83ed034980d9257b4c
Mikael-Aare <mikaelaare@icloud.com>
2021-03-21 20:02:17 +0100

    Made volume random, for fun

--- commands/utility/volume.js
+++ commands/utility/volume.js
@@ -5,12 +5,26 @@ module.exports = {
 	callback: async ({ message, args, text, client, prefix, instance, interaction }) => {
 		
 		const { channel } = message.member.voice;
+
 		if (!channel) return message.channel.send('I\'m sorry but you need to be in a voice channel to play music!');
+		
 		const serverQueue = message.client.queue.get(message.guild.id);
+
 		if (!serverQueue) return message.channel.send('There is nothing playing.');
+
 		if (!args[0]) return message.channel.send(`The current volume is: **${serverQueue.volume}**`);
-		serverQueue.volume = args[0]; // eslint-disable-line
-		serverQueue.connection.dispatcher.setVolumeLogarithmic(args[0] / 5);
-		return message.channel.send(`I set the volume to: **${args[0]}**`);
+
+		const volume = args[0] + 1;
+
+		// Changes volume somewhere between the number specified and 0;
+		serverQueue.volume = Math.floor(Math.random() * volume);
+
+		volume = serverQueue.volume;
+
+//		serverQueue.volume = args[0];
+
+		serverQueue.connection.dispatcher.setVolumeLogarithmic(volume / 5);
+
+		return message.channel.send('I set the volume to: **' + volume + '**');
 	}
 };
