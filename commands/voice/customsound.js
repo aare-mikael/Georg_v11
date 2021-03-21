63c9cdaf7f89000ba1c329f63da104f77b1bd851
Mikael-Aare <mikaelaare@icloud.com>
2021-03-21 17:58:52 +0100

    dev work

--- commands/voice/customsound.js
+++ commands/voice/customsound.js
@@ -1,73 +1,72 @@
-var path = require("path")
-const fs = require("fs")
-const usersPath = require('../../customsounds');
-const introSoundAdd = require('../../utilities/introSoundAdd');
-const introSoundDel = require('../../utilities/introSoundDel');
+// var path = require("path")
+// const fs = require("fs")
+// const usersPath = (path.join(__dirname, '..', 'customsounds/'));
+// const introSoundAdd = require('../../utilities/introSoundAdd');
+// const introSoundDel = require(path.join(__dirname, '..', 'utilities/introSoundDel.js'));
 
-module.exports = {
-	name: 'customsound',
-	description: "Choose a sound you want to play when you enter a voice channel.",
-        category: "Voice",
-	args: true,
-	callback: ({ message }) => {
+// module.exports = {
+// 	name: 'customsound',
+// 	description: "Choose a sound you want to play when you enter a voice channel.",
+//         category: "Voice",
+// 	callback: ({ message }) => {
         
-                var name = message.author.tag;
+//                 var name = message.author.tag;
 
-                const embed = require(path.join(__dirname, '../utilities', 'CustomsoundEmbed.js'));
+//                 const embed = require(path.join(__dirname, '../utilities', 'CustomsoundEmbed.js'));
 
-                if (!args[0]) {
+//                 if (!args[0]) {
 
-                        message.channel.send(embed(message, args));
+//                         message.channel.send(embed(message, args));
 
-                }                        
+//                 }                        
 
-                switch(args[0]){
+//                 switch(args[0]){
 
-                        case 'add':
-                                var url = args[1]
-                                if(fs.existsSync(usersPath + name + ".js")){
-                                var intro = require(path.join(usersPath + name +".js"))
-                                if( intro.id !== message.author.id ){
-                                message.channel.send("You can't change other users intro-sound!")
-                                return
-                                } else {
-                                  introSoundDel(client, message);
-                                  introSoundAdd(client, message, name, url, true)
-                                  message.reply('if you had an intro sound previously, you have to delete it first, and then add your new sound! Your intro-sound is saved, and available for use!')
-                                }
-                                } else {
-                                  introSoundAdd(client, message, name, url, true)
-                                  message.reply("if you had an intro sound previously, you have to delete it first, and then add your new sound! Your intro-sound is saved, and available for use!")
-                                }
-                        break;
+//                         case 'add':
+//                                 var url = args[1]
+//                                 if(fs.existsSync(usersPath + name + ".js")){
+//                                 var intro = require(path.join(usersPath + name +".js"))
+//                                 if( intro.id !== message.author.id ){
+//                                 message.channel.send("You can't change other users intro-sound!")
+//                                 return
+//                                 } else {
+//                                   introSoundDel(client, message);
+//                                   introSoundAdd(client, message, name, url, true)
+//                                   message.reply('if you had an intro sound previously, you have to delete it first, and then add your new sound! Your intro-sound is saved, and available for use!')
+//                                 }
+//                                 } else {
+//                                   introSoundAdd(client, message, name, url, true)
+//                                   message.reply("if you had an intro sound previously, you have to delete it first, and then add your new sound! Your intro-sound is saved, and available for use!")
+//                                 }
+//                         break;
 
-                        case 'delete':
-                                if(fs.existsSync(usersPath + name + ".js")){
-                                        introSoundDel(client, message);
-                                        }
-                                        message.reply("your sound is deleted.")
-                        break;
+//                         case 'delete':
+//                                 if(fs.existsSync(usersPath + name + ".js")){
+//                                         introSoundDel(client, message);
+//                                         }
+//                                         message.reply("your sound is deleted.")
+//                         break;
 
-                        case 'toggleon':
+//                         case 'toggleon':
                         
-                                if(fs.existsSync(usersPath + name + ".js")){
-                                        var intro = require(path.join(usersPath + name +".js"))     
-                                        introSoundDel(client, message);
-                                        introSoundAdd(client, message, name, url, true)
-                                        message.reply("your sound is toggled on and will play from now on.")
-                                }
+//                                 if(fs.existsSync(usersPath + name + ".js")){
+//                                         var intro = require(path.join(usersPath + name +".js"))     
+//                                         introSoundDel(client, message);
+//                                         introSoundAdd(client, message, name, url, true)
+//                                         message.reply("your sound is toggled on and will play from now on.")
+//                                 }
 
-                        break;
+//                         break;
 
-                        case 'toggleoff':
+//                         case 'toggleoff':
 
-                                if(fs.existsSync(usersPath + name + ".js")){
-                                        var intro = require(path.join(usersPath + name +".js"))     
-                                        introSoundDel(client, message);
-                                        introSoundAdd(client, message, name, url, false)
-                                        message.reply("your sound is toggled off, and will not play from now on.")
-                                }
+//                                 if(fs.existsSync(usersPath + name + ".js")){
+//                                         var intro = require(path.join(usersPath + name +".js"))     
+//                                         introSoundDel(client, message);
+//                                         introSoundAdd(client, message, name, url, false)
+//                                         message.reply("your sound is toggled off, and will not play from now on.")
+//                                 }
 
-                        }
-	},
-};
+//                         }
+// 	},
+// };
