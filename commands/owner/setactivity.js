e3b470c685926c7579241512524837c195f4a639
Mikael-Aare <mikaelaare@icloud.com>
2021-03-22 13:47:44 +0100

    Created setactivity.js

---
+++ commands/owner/setactivity.js
@@ -0,0 +1,22 @@
+module.exports = {
+    name: 'setstatus',
+    description: "Sets my activity, but only my maker can use this!",
+    category: "Private",
+    ownerOnly: true,
+    callback: async ({ message, args, text, client, prefix, instance, channel, interaction }) => {
+
+        if (!args) {
+            message.channel.send('Available types are; "PLAYING", "STREAMING", "LISTENING", "WATCHING", and "COMPETING".');
+            return;
+        }
+
+        let filter = m => (m.author.id === message.author.id); 
+        let collectedActivity = await message.channel.awaitMessages(filter, { max: 1 });
+        
+        let filter2 = m => (m.author.id === message.author.id);
+        let collectedType = await message.channel.awaitMessages(filter2, { max: 1 });
+
+        client.user.setActivity(collectedActivity, { type: collectedType });
+    },
+  }    
+  
