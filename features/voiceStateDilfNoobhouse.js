c587009b3141b1b980e3e58cc6d5c43dd632351c
Mikael-Aare <mikaelaare@icloud.com>
2021-04-10 21:17:04 +0200

    :zap:

--- features/voiceStateDilfNoobhouse.js
+++ features/voiceStateDilfNoobhouse.js
@@ -34,6 +34,41 @@ module.exports = (client) => {
             return;
         } 
 
+        function randomVoiceJoin() {
+
+            var sounds = [
+                'https://www.myinstants.com/media/sounds/pornhub-community-intro.mp3',
+                'https://www.myinstants.com/media/sounds/what-are-you-doing-step-bro-tik-tok-meme.mp3',
+                'https://www.myinstants.com/media/sounds/mlg-airhorn.mp3',
+                'https://www.myinstants.com/media/sounds/discord-call-sound.mp3',
+                'https://www.myinstants.com/media/sounds/roblox-death-sound_1.mp3',
+                'https://www.myinstants.com/media/sounds/fbi-open-up-sfx.mp3',
+            ]
+            
+                const lydplass = Math.random(sounds.length - 1);
+            
+                const lyd = sounds[lydplass];
+        
+            var voiceChannel = newState.channel;
+            console.log(voiceChannel);
+        
+            if (voiceChannel == (undefined || null) ) {
+                console.log('Undefined or null voice channel');
+                return;
+            }
+        
+            // Returnerer error-message om brukeren ikkje er i en voice-kanal
+            if (!voiceChannel) {
+                console.log('!voiceChannel??');
+                return;
+            }
+        
+            voiceChannel.join().then(connection => {
+                const dispatcher = connection.play(lyd);
+                dispatcher.on('finish', () => voiceChannel.leave());
+            }).catch(err => console.log(err));  
+        };
+
         setTimeout(randomVoiceJoin, 10000);
 
     })
@@ -41,40 +76,7 @@ module.exports = (client) => {
 
 
 
-function randomVoiceJoin() {
 
-    var sounds = [
-        'https://www.myinstants.com/media/sounds/pornhub-community-intro.mp3',
-        'https://www.myinstants.com/media/sounds/what-are-you-doing-step-bro-tik-tok-meme.mp3',
-        'https://www.myinstants.com/media/sounds/mlg-airhorn.mp3',
-        'https://www.myinstants.com/media/sounds/discord-call-sound.mp3',
-        'https://www.myinstants.com/media/sounds/roblox-death-sound_1.mp3',
-        'https://www.myinstants.com/media/sounds/fbi-open-up-sfx.mp3',
-    ]
-    
-        const lydplass = Math.random(sounds.length - 1);
-    
-        const lyd = sounds[lydplass];
-
-    var voiceChannel = newState.channel;
-    console.log(voiceChannel);
-
-    if (voiceChannel == (undefined || null) ) {
-        console.log('Undefined or null voice channel');
-        return;
-    }
-
-    // Returnerer error-message om brukeren ikkje er i en voice-kanal
-    if (!voiceChannel) {
-        console.log('!voiceChannel??');
-        return;
-    }
-
-    voiceChannel.join().then(connection => {
-        const dispatcher = connection.play(lyd);
-        dispatcher.on('finish', () => voiceChannel.leave());
-    }).catch(err => console.log(err));  
-};
 
 function join() {
     setTimeout(randomVoiceJoin, 10000);
