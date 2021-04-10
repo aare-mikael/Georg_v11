e23b137592ad89989491cbc789f73eb509b24d98
Mikael-Aare <mikaelaare@icloud.com>
2021-04-10 21:09:44 +0200

    :zap: :zap:

--- features/voiceStateDilfNoobhouse.js
+++ features/voiceStateDilfNoobhouse.js
@@ -25,37 +25,44 @@ module.exports = (client) => {
         var newserver = newState.guild.id.toString();
         var oldserver = oldState.guild.id.toString();
 
-        if (newserver != DilfNoobhouse || team10Discord || georgland ) return;
+        if (newserver != (DilfNoobhouse || team10Discord || georgland) ) return;
 
         // checks if Georg is currently playing any sound in the relevant guild, and stopping the introSound if so;
         const serverQueue = newState.client.queue.get(newState.guild.id);
         
         if (serverQueue) {
             return;
-        } else {
+        } 
+
             join();
-        }
+
     })
 }
 
-var sounds = [
-    'https://www.myinstants.com/media/sounds/pornhub-community-intro.mp3',
-    'https://www.myinstants.com/media/sounds/what-are-you-doing-step-bro-tik-tok-meme.mp3',
-    'https://www.myinstants.com/media/sounds/mlg-airhorn.mp3',
-    'https://www.myinstants.com/media/sounds/discord-call-sound.mp3',
-    'https://www.myinstants.com/media/sounds/roblox-death-sound_1.mp3',
-    'https://www.myinstants.com/media/sounds/fbi-open-up-sfx.mp3',
-]
 
-    const lydplass = Math.random(sounds.length - 1);
-
-    const lyd = sounds[lydplass];
 
 function randomVoiceJoin() {
 
-    var voiceChannel = newState.channel;
+    var sounds = [
+        'https://www.myinstants.com/media/sounds/pornhub-community-intro.mp3',
+        'https://www.myinstants.com/media/sounds/what-are-you-doing-step-bro-tik-tok-meme.mp3',
+        'https://www.myinstants.com/media/sounds/mlg-airhorn.mp3',
+        'https://www.myinstants.com/media/sounds/discord-call-sound.mp3',
+        'https://www.myinstants.com/media/sounds/roblox-death-sound_1.mp3',
+        'https://www.myinstants.com/media/sounds/fbi-open-up-sfx.mp3',
+    ]
+    
+        const lydplass = Math.random(sounds.length - 1);
+    
+        const lyd = sounds[lydplass];
 
-    if (voiceChannel == (undefined || null) ) return;
+    var voiceChannel = newState.channel;
+    console.log(voiceChannel);
+
+    if (voiceChannel == (undefined || null) ) {
+        console.log('Undefined or null voice channel');
+        return;
+    }
 
     // Returnerer error-message om brukeren ikkje er i en voice-kanal
     if (!voiceChannel) {
