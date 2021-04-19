a68209ecbaa6128f4eb3d435c2c90d24bee084a9
aare-mikael <aaremikael@gmail.com>
2021-04-19 15:31:11 +0200

    :zap: :zap:

--- features/voiceStateDilfNoobhouse.js
+++ features/voiceStateDilfNoobhouse.js
@@ -1,102 +1,102 @@
-const mongo = require('../utilities/mongoutilities/mongo');
-const mongoose = require('mongoose');
-const discordUsers = require('../mongoschemas/discordUsers');
+// const mongo = require('../utilities/mongoutilities/mongo');
+// const mongoose = require('mongoose');
+// const discordUsers = require('../mongoschemas/discordUsers');
 
-module.exports = (client) => {
+// module.exports = (client) => {
 
-    // Makes the bot pay attention to whenever somebody joins a new channel;
-    client.on('voiceStateUpdate', async (oldState, newState) => {
+//     // Makes the bot pay attention to whenever somebody joins a new channel;
+//     client.on('voiceStateUpdate', async (oldState, newState) => {
 
-        const georg = '741703921877123164';
+//         const georg = '741703921877123164';
 
-        const team10Discord = '612947002853949458';
-        const georgland = '710444747298439168';
-        const DilfNoobhouse = '634849156179165222';
+//         const team10Discord = '612947002853949458';
+//         const georgland = '710444747298439168';
+//         const DilfNoobhouse = '634849156179165222';
 
-        var servers = [ '612947002853949458', '710444747298439168', '634849156179165222', ];
+//         var servers = [ '612947002853949458', '710444747298439168', '634849156179165222', ];
 
-        // if (newState.channel.members.filter(m => m.user.id == "123456789")) {
-        //     console.log("User 123456789 is in the channel");
-        // } else {
-        //     console.log("User 123456789 is NOT in the channel");
-        // }
+//         // if (newState.channel.members.filter(m => m.user.id == "123456789")) {
+//         //     console.log("User 123456789 is in the channel");
+//         // } else {
+//         //     console.log("User 123456789 is NOT in the channel");
+//         // }
 
-        if (newState.guild.voiceConnection) return;
+//         if (newState.guild.voiceConnection) return;
 
-        if (newState.channel == null || undefined) return;
+//         if (newState.channel == null || undefined) return;
 
-        // Variable names should tell you what this does;
-        var newserver = newState.guild.id.toString();
-        var oldserver = oldState.guild.id.toString();
+//         // Variable names should tell you what this does;
+//         var newserver = newState.guild.id.toString();
+//         var oldserver = oldState.guild.id.toString();
 
-        if (!servers.includes(newserver)) return;
+//         if (!servers.includes(newserver)) return;
 
-        function sleep(milliseconds) {
-            const date = Date.now();
-            let currentDate = null;
-            do {
-              currentDate = Date.now();
-            } while (currentDate - date < milliseconds);
-          }
+//         function sleep(milliseconds) {
+//             const date = Date.now();
+//             let currentDate = null;
+//             do {
+//               currentDate = Date.now();
+//             } while (currentDate - date < milliseconds);
+//           }
 
-        // Collects the id of the person joining;
-        const newPerson = newState.member.id.toString();
+//         // Collects the id of the person joining;
+//         const newPerson = newState.member.id.toString();
 
-        if (newPerson == georg) {
-            return;
-        }
+//         if (newPerson == georg) {
+//             return;
+//         }
 
-          console.log('Being difficult for 5 seconds');
-          sleep(5000);
-          console.log('Being normal now');
+//           console.log('Being difficult for 5 seconds');
+//           sleep(5000);
+//           console.log('Being normal now');
 
-        // checks if Georg is currently playing any sound in the relevant guild, and stopping the introSound if so;
-        const serverQueue = newState.client.queue.get(newState.guild.id);
+//         // checks if Georg is currently playing any sound in the relevant guild, and stopping the introSound if so;
+//         const serverQueue = newState.client.queue.get(newState.guild.id);
         
-        if (serverQueue) {
-            return;
-        } 
+//         if (serverQueue) {
+//             return;
+//         } 
 
-        var sounds = [
-            'https://www.myinstants.com/media/sounds/pornhub-community-intro.mp3',
-            'https://www.myinstants.com/media/sounds/what-are-you-doing-step-bro-tik-tok-meme.mp3',
-// Too obnoxious :)            'https://www.myinstants.com/media/sounds/mlg-airhorn.mp3',
-            'https://www.myinstants.com/media/sounds/discord-call-sound.mp3',
-            'https://www.myinstants.com/media/sounds/roblox-death-sound_1.mp3',
-            'https://www.myinstants.com/media/sounds/fbi-open-up-sfx.mp3',
-        ]
+//         var sounds = [
+//             'https://www.myinstants.com/media/sounds/pornhub-community-intro.mp3',
+//             'https://www.myinstants.com/media/sounds/what-are-you-doing-step-bro-tik-tok-meme.mp3',
+// // Too obnoxious :)            'https://www.myinstants.com/media/sounds/mlg-airhorn.mp3',
+//             'https://www.myinstants.com/media/sounds/discord-call-sound.mp3',
+//             'https://www.myinstants.com/media/sounds/roblox-death-sound_1.mp3',
+//             'https://www.myinstants.com/media/sounds/fbi-open-up-sfx.mp3',
+//         ]
             
-        const lydplass = Math.floor(Math.random() * (sounds.length - 1));
+//         const lydplass = Math.floor(Math.random() * (sounds.length - 1));
 
-        console.log('Lydplass: ' + lydplass);
+//         console.log('Lydplass: ' + lydplass);
     
-        const lyd = sounds[lydplass];
+//         const lyd = sounds[lydplass];
 
-        console.log('lyd: ' + lyd);
+//         console.log('lyd: ' + lyd);
         
-        var voiceChannel = newState.channel;
+//         var voiceChannel = newState.channel;
     
-        if (voiceChannel == (undefined || null) ) {
-            console.log('Undefined or null voice channel');
-            return;
-        }
+//         if (voiceChannel == (undefined || null) ) {
+//             console.log('Undefined or null voice channel');
+//             return;
+//         }
         
-        // Returnerer error-message om brukeren ikkje er i en voice-kanal
-        if (!voiceChannel) {
-            console.log('!voiceChannel??');
-            return;
-        }
+//         // Returnerer error-message om brukeren ikkje er i en voice-kanal
+//         if (!voiceChannel) {
+//             console.log('!voiceChannel??');
+//             return;
+//         }
         
-        voiceChannel.join().then(connection => {
-            const dispatcher = connection.play(lyd);
-            dispatcher.on('finish', () => voiceChannel.leave());
-        }).catch(err => console.log(err));  
+//         voiceChannel.join().then(connection => {
+//             const dispatcher = connection.play(lyd);
+//             dispatcher.on('finish', () => voiceChannel.leave());
+//         }).catch(err => console.log(err));  
 
-    })
-}
+//     })
+// }
 
-module.exports.config = {
-    displayName: "Georg's database",
-    dbName: 'Georg-DB',
-    loadDBfirst: true,
-}
+// module.exports.config = {
+//     displayName: "Georg's database",
+//     dbName: 'Georg-DB',
+//     loadDBfirst: true,
+// }
